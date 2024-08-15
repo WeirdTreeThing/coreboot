44a19b7cc0d84b6db0b6968eb47fa4d38e2282cd
WeirdTreeThing <bradyn127@protonmail.com>
2024-08-14 20:24:15 -0400

    A few changes
    I honestly dont remember it has been many months

--- src/mainboard/asus/maximus_xi_hero/ramstage.c
+++ src/mainboard/asus/maximus_xi_hero/ramstage.c
@@ -3,7 +3,25 @@
 #include <soc/ramstage.h>
 #include "include/gpio.h"
 
-void mainboard_silicon_init_params(FSPS_UPD *params)
+static void init_mainboard(void *chip_info)
 {
         gpio_configure_pads(gpio_table, ARRAY_SIZE(gpio_table));
+}
+
+struct chip_operations mainboard_ops = {
+        .init = init_mainboard,
+};
+
+void mainboard_silicon_init_params(FSPS_UPD *params)
+{
+
+	// Disable DMI ASPM
+	//params->DmiAspm = 0;
+	//params->PchDmiAspmCtrl = 0;
+	//params->PchLegacyIoLowLatency = 1;
+
+	// Disable Intel AMT
+	//params->AmtEnabled = 0;
+	//params->AmtKvmEnabled = 0;
+	//params->AmtSolEnabled = 0;
 }
