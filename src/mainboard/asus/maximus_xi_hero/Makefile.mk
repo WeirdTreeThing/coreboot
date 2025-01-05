c5c1984b3571b9d1edb7bc3673c9b40347fa4a1a
Alicja Michalska <alicja.michalska@9elements.com>
2025-01-05 02:12:15 +0100

    mb/asus/maximus_ix_hero: Few ideas

--- src/mainboard/asus/maximus_xi_hero/Makefile.mk
+++ src/mainboard/asus/maximus_xi_hero/Makefile.mk
@@ -1,6 +1,4 @@
 ## SPDX-License-Identifier: GPL-2.0-only
 
 romstage-y += romstage.c
-
 ramstage-y += ramstage.c
-# ramstage-$(CONFIG_SOC_INTEL_COMMON_BLOCK_HDA_VERB) += variants/$(VARIANT_DIR)/hda_verb.c
--- src/mainboard/asus/maximus_xi_hero/romstage.c
+++ src/mainboard/asus/maximus_xi_hero/romstage.c
@@ -4,59 +4,32 @@
 #include <soc/romstage.h>
 
 static const struct cnl_mb_cfg memcfg = {
-        /* Access memory info through SMBUS. */
-	.spd[0] = {.read_type = NOT_EXISTING},
-        .spd[1] = {
-                .read_type = READ_SMBUS,
-                .spd_spec = {.spd_smbus_address = 0xa2},
-        },
-	.spd[2] = {.read_type = NOT_EXISTING},
-        .spd[3] = {
-                .read_type = READ_SMBUS,
-                .spd_spec = {.spd_smbus_address = 0xa6},
-        },
+	.spd[0] = {
+		.read_type = READ_SMBUS,
+		.spd_spec = {.spd_smbus_address = 0xa0},
+	},
+	.spd[1] = {
+		.read_type = READ_SMBUS,
+		.spd_spec = {.spd_smbus_address = 0xa2},
+	},
+	.spd[2] = {
+		.read_type = READ_SMBUS,
+		.spd_spec = {.spd_smbus_address = 0xa4},
+	},
+	.spd[3] = {
+		.read_type = READ_SMBUS,
+		.spd_spec = {.spd_smbus_address = 0xa6},
+	},
 
-        /*
-         * The dqs_map arrays map the ddr4 pins to the SoC pins
-         * for both channels.
-         *
-         * the index = pin number on ddr4 part
-         * the value = pin number on SoC
-         */
-        .dqs_map[DDR_CH0] = {0, 1, 4, 5, 2, 3, 6, 7},
-        .dqs_map[DDR_CH1] = {0, 1, 4, 5, 2, 3, 6, 7},
-
-        /* Baseboard uses 121, 81 and 100 rcomp resistors */
-        .rcomp_resistor = {121, 81, 100},
-
-	/* Baseboard is an interleaved design */
-        .dq_pins_interleaved = 1,
-
-        /*
-         * Baseboard Rcomp target values.
-         */
-        .rcomp_targets = {100, 40, 20, 20, 26},
-
-        /* Disable Early Command Training */
-        .ect = 0,
-
-        /* Base on board design */
-        .vref_ca_config = 2,
+	.rcomp_resistor = {121, 81, 100},
+	.dq_pins_interleaved = 1,
+	.vref_ca_config = 2,
 };
 
 void mainboard_memory_init_params(FSPM_UPD *memupd)
 {
-	memupd->FspmConfig.UserBd = 1;			// Board type: Desktop
-	//memupd->FspmConfig.DmiMaxLinkSpeed = 3;		// Force CPU <-> PCH link to run at Gen3 speed
-	//memupd->FspmConfig.EnableAbove4GBMmio = 1;	// Self-explanatory
-	memupd->FspmConfig.GttSize = 3;			// 8MB
-	memupd->FspmConfig.ApertureSize = 3;		// 512MB
-	memupd->FspmConfig.GtPsmiSupport = 0;
-	memupd->FspmConfig.IgdDvmt50PreAlloc = 2;	// 64MB
-
-	memupd->FspmConfig.SpdProfileSelected = 1;
-	memupd->FspmConfig.RefClk = 0;
-	memupd->FspmConfig.VddVoltage = 1350;
+	memupd->FspmConfig.DmiMaxLinkSpeed = 3;		// Force CPU <-> PCH link to run at Gen3 speed
+	memupd->FspmConfig.EnableAbove4GBMmio = 1;	// Self-explanatory
 
 	cannonlake_memcfg_init(&memupd->FspmConfig, &memcfg);
 }
