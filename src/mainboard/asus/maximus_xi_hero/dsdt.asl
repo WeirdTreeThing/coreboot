24389f140c21f8475120b067602635d489f44586
Alicja Michalska <alicja.michalska@9elements.com>
2025-01-05 02:12:15 +0100

    mb/asus/maximus_ix_hero: Few ideas

--- src/mainboard/asus/maximus_xi_hero/dsdt.asl
+++ src/mainboard/asus/maximus_xi_hero/dsdt.asl
@@ -1,7 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <acpi/acpi.h>
-
 DefinitionBlock(
 	"dsdt.aml",
 	"DSDT",
--- src/mainboard/asus/maximus_xi_hero/ramstage.c
+++ src/mainboard/asus/maximus_xi_hero/ramstage.c
@@ -1,7 +1,15 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <acpi/acpi.h>
+#include <device/device.h>
 #include <soc/ramstage.h>
-#include "include/gpio.h"
+#include "gpio.h"
+
+void mainboard_fill_fadt(acpi_fadt_t *fadt)
+{
+	fadt->preferred_pm_profile = PM_DESKTOP;
+	fadt->iapc_boot_arch |= ACPI_FADT_LEGACY_DEVICES | ACPI_FADT_8042;
+}
 
 static void init_mainboard(void *chip_info)
 {
@@ -14,14 +22,7 @@ struct chip_operations mainboard_ops = {
 
 void mainboard_silicon_init_params(FSPS_UPD *params)
 {
-
-	// Disable DMI ASPM
-	//params->DmiAspm = 0;
-	//params->PchDmiAspmCtrl = 0;
-	//params->PchLegacyIoLowLatency = 1;
-
-	// Disable Intel AMT
-	//params->AmtEnabled = 0;
-	//params->AmtKvmEnabled = 0;
-	//params->AmtSolEnabled = 0;
+	params->DmiAspm = 0;
+	params->PchDmiAspmCtrl = 0;
+	params->PchLegacyIoLowLatency = 1;
 }
